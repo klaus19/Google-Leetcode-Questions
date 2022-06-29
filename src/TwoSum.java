8cae4d42629b569e6c9dac230b759bf116d17129
tejaskhartude <tejaskhartude@gmail.com>
2022-06-29 15:28:53 +0530

    Added new problem of Two Sum

---
+++ src/TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public int[]getTwoSum(int[]arr,int target) {
+
+        Map<Integer, Integer> values = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+            if (values.containsKey(complement)) {
+                return new int[]{values.get(complement), i};
+            }
+            values.put(arr[i], i);
+        }
+        throw new IllegalArgumentException("No match found");
+    }
+
+
+    public static void main(String[] args) {
+        TwoSum ts = new TwoSum();
+        int target = 7;
+        int[]nums = new int[]{1,2,5,6};
+
+        System.out.println(ts.getTwoSum(nums,target));
+    }
+}
