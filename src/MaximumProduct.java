aeb7b321f637898caf8c273357a827b350a5b16d
tejaskhartude <tejaskhartude@gmail.com>
2022-06-30 10:35:18 +0530

    added Maximum product

--- src/MaximumProduct.java
+++ src/MaximumProduct.java
@@ -1,39 +1,31 @@
+import org.jetbrains.annotations.NotNull;
+
 public class MaximumProduct {
 
-       public int getMax_product(int[]arr){
-
-           int ans=arr[0];
-           int min_product=ans;
-           int max_product=ans;
-for (int i=1;i<arr.length;i++){
-    if (max_product<0){
-        swap(min_product,max_product);
-    }
-
-    max_product = Math.max(arr[i],max_product*arr[i]);
-    min_product = Math.min(arr[i],min_product*arr[i]);
-    ans = Math.max(max_product,ans);
-
-}
-           return ans;
-}
+       public int getMax_product(@NotNull int[]arr) {
 
 
-    private int swap(int min_product, int max_product) {
+           int min_product = arr[0];
+           int max_product = arr[0];
+           int ans = max_product;
 
-           int temp;
-           temp = min_product;
-           min_product=max_product;
-           max_product=temp;
-           return max_product;
-    }
+           for (int i=1;i<arr.length;i++){
+               int tempMax = Math.max(arr[i],Math.max(arr[i]*max_product,arr[i]*min_product));
+               min_product = Math.min(arr[i],Math.min(arr[i]*max_product,arr[i]*min_product));
+               max_product=tempMax;
+               ans = Math.max(max_product,ans);
+           }
+return ans;
+       }
 
     public static void main(String[] args) {
 
            MaximumProduct ms = new MaximumProduct();
-           int[]arr = new int[]{2,3,-2,1};
+           int[]arr = new int[]{2,-3,-2,1};
+           int[]nums = new int []{4,4,-2,-4};
 
            System.out.println(ms.getMax_product(arr));
+           System.out.println(ms.getMax_product(nums));
 
     }
 }
