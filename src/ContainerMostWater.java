b809f74a68eaefd1391d8ae4dc5fce19e8100816
tejaskhartude <tejaskhartude@gmail.com>
2022-07-01 15:08:45 +0530

    Updated

--- src/ContainerMostWater.java
+++ src/ContainerMostWater.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ContainerMostWater {
+public class ContainerMostWater {
+
+    public int maxArea(int[]height){
+
+        int max_area =0;
+        int a_pointer=0;
+        int b_pointer= height.length-1;
+
+        while (a_pointer<b_pointer){
+            if(height[a_pointer]<height[b_pointer]){
+                max_area = Math.max(max_area,height[a_pointer]*(b_pointer-a_pointer));
+                a_pointer+=1;
+            }else{
+                max_area = Math.max(max_area,height[b_pointer]*(b_pointer-a_pointer));
+                b_pointer--;
+            }
+        }
+          return max_area;
+    }
 }
--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ThreeSum {
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreeSum {
+
+    public List<List<Integer>>threeSum(int[]nums){
+
+        Arrays.sort(nums);
+        List<List<Integer>>output = new LinkedList<>();
+
+        for (int i=0;i<nums.length-2;i++){
+            
+        }
+    }
 }
