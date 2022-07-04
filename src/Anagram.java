d20eadd327ed472b82d0d6583e56c3118ccece5f
tejaskhartude <tejaskhartude@gmail.com>
2022-07-04 15:21:01 +0530

    Added Anagram problem

---
+++ src/Anagram.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+
+public class Anagram {
+
+    public boolean getAnagram(String str,String s){
+
+         char[]ch1 = str.toCharArray();
+         char[]ch2 = s.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        String string1 = new String(ch1);
+        String string2 = new String(ch2);
+
+        return string1.contentEquals(string2);
+    }
+
+    public static void main(String[] args) {
+        Anagram a = new Anagram();
+        String s = "LIST";
+        String t = "TSLI";
+
+        System.out.println(a.getAnagram(s,t));
+    }
+}
