1287445afff12e0d1727f75dcfd243d300022243
tejaskhartude <tejaskhartude@gmail.com>
2022-07-20 16:17:18 +0530

    Added new problem ReverseLinkedList

---
+++ src/ListNode.java
@@ -0,0 +1,23 @@
+
+public class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){val=x;}
+}
+ class ReverseLinkedList {
+
+    public ListNode reversel(ListNode head){
+
+        ListNode prev = null;
+
+        while (head!=null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+
+        }
+         return prev;
+    }
+
+}
