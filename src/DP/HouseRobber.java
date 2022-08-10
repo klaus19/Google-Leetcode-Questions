e27b8efb11f415104b639bb91732d85bcf74ed7e
tejaskhartude <tejaskhartude@gmail.com>
2022-08-10 12:52:24 +0530

    Added House Robber question

--- src/DP/HouseRobber.java
+++ src/DP/HouseRobber.java
@@ -22,6 +22,22 @@ public class HouseRobber {
         return curr_money;
     }
 
+    public int robbery(int nums[]){
+        int[]dp = new int[nums.length];
+
+        if (nums.length==1) return nums[0];
+        if (nums.length==2) return Math.max(nums[0],nums[1]);
+
+        dp[0]=nums[0];
+        dp[1] = Math.max(nums[1],dp[0]);
+
+        for (int i=2;i<nums.length;i++){
+            dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
+
+        }
+        return dp[nums.length-1];
+    }
+
     public static void main(String[] args) {
         HouseRobber hp = new HouseRobber();
         int[]money = new int[]{2,7,9,3,1};
