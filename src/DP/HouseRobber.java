d7c95c5d882f49d9661f2ca9baaf97c6e24691aa
tejaskhartude <tejaskhartude@gmail.com>
2022-08-10 16:05:22 +0530

    Added House Robber 2 question

--- src/DP/HouseRobber.java
+++ src/DP/HouseRobber.java
@@ -7,36 +7,25 @@ package DP;
 //Time complexity
 public class HouseRobber {
 
-    public int rob(int[]nums){
-       if (nums.length==1){
-           return nums[0];
-       }
-       int money = nums[0];
-       int curr_money = Math.max(nums[0],nums[1]);
-       for (int i=2;i<nums.length;i++){
-           int new_money = Math.max(nums[i]+money,curr_money);
-
-           money=curr_money;
-           curr_money=new_money;
-       }
-        return curr_money;
-    }
-
-    public int robbery(int nums[]){
-        int[]dp = new int[nums.length];
-
-        if (nums.length==1) return nums[0];
-        if (nums.length==2) return Math.max(nums[0],nums[1]);
-
-        dp[0]=nums[0];
-        dp[1] = Math.max(nums[1],dp[0]);
-
-        for (int i=2;i<nums.length;i++){
-            dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
-
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
         }
-        return dp[nums.length-1];
+
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+         int dp[] = new int[nums.length];
+        dp[0] =nums[0];
+         dp[1] = Math.max(nums[0],nums[1]);
+
+         for (int i=2;i<dp.length;i++){
+             dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+
+         }
+         return dp[nums.length-1];
     }
+
 
     public static void main(String[] args) {
         HouseRobber hp = new HouseRobber();
