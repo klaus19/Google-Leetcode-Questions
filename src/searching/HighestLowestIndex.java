6ec40a313047c66070e93997006727c0d95f8d6b
tejaskhartude <tejaskhartude@gmail.com>
2022-08-19 19:25:48 +0530

    added the problem of finding hIGHEST AND lowest index occurance of the target number

--- src/searching/HighestLowestIndex.java
+++ src/searching/HighestLowestIndex.java
@@ -1,22 +1,62 @@
 package searching;
 
-import java.util.HashMap;
-import java.util.Map;
+
+// Time cOMPLEXITY o(log n)
+
+//Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+//
+//If target is not found in the array, return [-1, -1].
+
 
 public class HighestLowestIndex {
-
+    int[]result = new int[2];
     public int[] searchRange(int[] nums, int target) {
 
-        Map<Integer,Integer>hmp = new HashMap<>();
 
+        result[0] = startingIndex(nums,target);
+        result[1] = endingIndex(nums,target);
 
-        for (int i=0;i<nums.length;i++) {
-            if (!hmp.containsKey(nums[i] == target)) {
-                return new int[]{nums[i], i};
-            } else {
-                //  hmp.put()
-            }
-        }
-        return nums;
+       return result;
     }
+    private int startingIndex(int[] nums, int target) {
+        int index=-1;
+        int start=0;
+        int end=nums.length-1;
+
+        while (start<=end){
+            int mid = start + (end-start)/2 ;
+            if (nums[mid]>=target){
+                end=mid-1;
+            }else{
+                start= mid+1;
+            }
+
+
+            if (nums[mid]==target) index=mid;
+        }
+        return index;
+    }
+
+    private int endingIndex(int[] nums, int target) {
+
+        int index=-1;
+        int start=0;
+        int end=nums.length-1;
+
+        while (start<=end){
+            int mid = start + (end-start)/2 ;
+
+            if (nums[mid]<=target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+
+            if (nums[mid]==target) index=mid;
+        }
+
+        return index;
+    }
+
+
 }
