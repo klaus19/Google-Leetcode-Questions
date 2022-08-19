685dc6cb48b47daab7f75ed041bb60ad13deb8e0
tejaskhartude <tejaskhartude@gmail.com>
2022-08-19 15:33:07 +0530

    MaximumProduct

--- src/DP/MaxiumumProduct.java
+++ src/DP/MaxiumumProduct.java
@@ -4,14 +4,19 @@ public class MaxiumumProduct {
 
     public int maximumProduct(int[]nums){
 
-        int max= nums[0];
-        int curr = max;
+        int max_ending= nums[0];
+
+        int min_ending = nums[0];
+
+        int max_so_far = nums[0];
 
         for (int i=1;i<nums.length;i++){
-            max = Math.max(max*nums[i],max);
-            curr = Math.max(curr,max);
+            int temp = Math.max(Math.max(nums[i],nums[i]*max_ending),nums[i]*min_ending);
+            min_ending = Math.min(Math.min(nums[i],nums[i]*max_ending),nums[i]*min_ending);
+            max_ending=temp;
+            max_so_far = Math.max(max_so_far,max_ending);
         }
-        return curr;
+        return max_so_far;
     }
 
     public static void main(String[] args) {
