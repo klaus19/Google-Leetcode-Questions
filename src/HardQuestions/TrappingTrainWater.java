8a1ca34a961a767e16d55cf821e2bae987d40624
tejaskhartude <tejaskhartude@gmail.com>
2022-08-21 10:22:21 +0530

    Added trapping Rainwater problem

--- src/HardQuestions/TrappingTrainWater.java
+++ src/HardQuestions/TrappingTrainWater.java
@@ -1,6 +1,11 @@
 package HardQuestions;
 
 
+//Given n non-negative integers representing an elevation map where the width of each bar is 1,
+// compute how much water it can trap after raining
+
+//
+
 public class TrappingTrainWater {
 
     public int trappingWater(int[]height){
